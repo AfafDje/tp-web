46b8f55eb3cc0a3a3ec7347796e38f6439511026
Afaf <160837623+AfafDje@users.noreply.github.com>
2025-03-03 02:15:42 +1300

    Add files via upload

---
+++ interface.php
@@ -0,0 +1,41 @@
+<html lang="ar">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>متجر الكتب</title>
+    <link rel="stylesheet" href="interface.css">
+    <script src=interface.js></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://i.pinimg.com/736x/db/7a/dd/db7add51c23e6b3d5c84c791eee9c415.jpg">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+  </head>
+  <body>
+    <div class="home">
+      <nav>
+        <a href="#" class="logo">
+          <i class="fa-solid fa-book"></i>
+          <h3>bookly</h3>
+        </a>
+        <div class="nav">
+          <a href="#">home</a>
+          <a href="#">categories</a>
+          <a href="#">products</a>
+          <a href="#">about </a>
+          <a href="#">contacts </a>
+        </div>
+      </nav>
+    </div>
+    <div class="div"></div>
+    <section class="homepage">
+      <div class="content">
+        <h2>SAVE ON ALL PRODUCTS</h2>
+        <h1>75% OFF</h1>
+        <h3>WHEN YOU PURCHASE 2 OR MORE ITEMS*</h3>
+        <button>SEOP NOW </button>
+      </div>
+    </section> 
+  </body>
+</html>
+.
